ceed124828bd5a31142dd1c5087f3aca5785d871
changhyun1.lee <changhyun1.lee@samsung.com>
2015-02-03 23:20:43 +0900

    Added login module

--- app.js
+++ app.js
@@ -8,30 +8,85 @@ var app = express();
 var db = require('mongojs').connect('rest', ['users', 'markers']);
 
 app.use(logger());
+app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 //app.use(express.session());
 //app.use(expreess.static('public'))
 
-/* login
+/*
+ * Create user
+  */
 app.post('/users', function(request, response) {
-    db.users.insert({
-        title: 'test'
-    }, function (error, results) {
-        if (error) {
-            response.send(500);
-        } else {
-            response.send(results);
-        }
-    });
+    var email = request.body.email;
+    var password = request.body.password;
+    console.log(email)
+    console.log(password)
+    if (email && password) {
+        // find duplicated user
+        db.users.findOne({
+            email: email
+        }, function (error, result) {
+                if (result) {
+                    response.send(409);
+                } else {
+                    db.users.insert({
+                        email: email,
+                        password: password,
+                        markers: []
+                    }, function (error, result) {
+                        if (error) {
+                            response.send(500);
+                        } else {
+                            response.send(result);
+                        }
+                    });
+                }
+            }
+        );
+    } else {
+        response.send(400);
+    }
 });
-*/
 
+/*
+ * Login
+ */
+app.post('/users/login', function(request, response) {
+    var email = request.body.email;
+    var password = request.body.password;
+    if (email && password) {
+        // find duplicated user
+        db.users.findOne({
+                email: email
+            }, function (error, result) {
+                if (result) {
+                    if (password === result.password) {
+                        response.send({message: 'login success'}, 200);
+                    } else {
+                        response.send({message: 'invalid pasword'}, 400);
+                    }
+                } else {
+                    response.send(409);
+                }
+            }
+        );
+    } else {
+        response.send(400);
+    }
+});
+
+/*
+ * Find all user
+ */
 app.get('/users', function(request, response) {
     db.users.find(function (error, results) {
         response.jsonp(results);
     });
 });
 
+/*
+ * Find user
+ */
 app.get('/users/:user', function(request, response) {
     db.collection('users').findOne({
         email: request.param('user'),
@@ -60,6 +115,9 @@ app.post('/markers', function(request, response) {
 });
 */
 
+/*
+ * Find all marker
+ */
 app.get('/markers', function(request, response) {
     db.markers.find(function (error, results) {
 		//response.writeHead(200, { 'Content-Type': 'application/json' }); 
@@ -67,6 +125,9 @@ app.get('/markers', function(request, response) {
     });
 });
 
+/*
+ * Find marker
+ */
 app.get('/markers/:marker', function(request, response) {
     db.collection('markers').findOne({
         _id: request.param('marker'),
